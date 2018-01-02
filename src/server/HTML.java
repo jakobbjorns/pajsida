33d1e72b8affaf0fe0d0198f535b900a3d1545cb
Jakob <jakob_b99@live.se>
2018-01-02 01:29:24 +0100

    Signed-off-by: Jakob <jakob_b99@live.se>

--- src/server/HTML.java
+++ src/server/HTML.java
@@ -1,10 +1,19 @@
 package server;
 import static spark.Spark.*;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 public class HTML {
 	public HTML() {
 		// TODO Auto-generated constructor stub
 		port(8080);
-		get("/hello", (req, res) -> "Hello World");
+		get("/hello", new Route() {
+			@Override
+			public Object handle(Request req, Response res) throws Exception {
+				return "Hello World";
+			}
+		});
 	}
 }

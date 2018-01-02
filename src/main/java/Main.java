c54050427e8c414d40b45aa8b716a0c5e1214e4c
Jakob Björns <jakob_b99@live.se>
2018-01-02 20:14:43 +0100

    SessionID

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import static spark.Spark.*;
 
-import javax.servlet.http.Cookie;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 import com.google.gson.*;
 
@@ -8,21 +10,30 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.util.Base64;
+
 
 public class Main {
 
 	/**
 	 * @param args
 	 */
+	SecureRandom secureRandom;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Main();
 	}
 	public Main() {
 		// TODO Auto-generated constructor stub
+		try {
+			secureRandom= SecureRandom.getInstance("SHA1PRNG");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		port(8181);
 
-		get("/post", new Route() {
+		get("/login", new Route() {
 			@Override
 			public Object handle(Request request, Response response) throws Exception {
 				System.out.println("GET-request " + request.protocol()+" from: "+request.headers("X-Real-IP")+" ("+request.ip()+")");
@@ -38,7 +49,7 @@ public class Main {
 				return response.body();
 			}
 		});
-		post("/post", new Route() {
+		post("/login", new Route() {
 			@Override
 			public Object handle(Request request, Response response) throws Exception {
 				//				for (String string : request.headers()) {
@@ -50,11 +61,10 @@ public class Main {
 
 					System.out.println(request.body());
 					response.body("HEJSAN");
-
-					response.cookie("name", "100000");
-					response.
+					response.cookie("", "", "sessionID", SessionID()+"", 3600, true, true);
+					response.redirect("https://bjorns.tk/admin");
 				}
-				
+
 				else{
 					response.body("forbidden");
 					response.status(403);
@@ -64,6 +74,19 @@ public class Main {
 				return response.body();
 			}
 		});
+	}
+	public  String SessionID () {
+		String id="";
+
+		try {
+			id=new String(Base64.getEncoder().encode(MessageDigest.getInstance("SHA-1").digest((secureRandom.nextInt()+"").getBytes())));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("SessionID: " + id);
+
+		return id;
 	}
 
 }

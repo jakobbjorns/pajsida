a33c576caceb585016cc6e4a2d4008200b5bc0af
jakob <jakob@Jakobs-Thinkpad-11e.lan>
2018-12-16 13:27:36 +0100

    iuy6treds

--- src/main/java/gojb/jbhttp/API/TempAPI.java
+++ src/main/java/gojb/jbhttp/API/TempAPI.java
@@ -7,11 +7,18 @@ import spark.Route;
 
 public class TempAPI {
 	static Route F56 = (request, response) ->{
-		ProcessBuilder pb = new ProcessBuilder("ssh", "glenn","'","curl","-s 192.168.1.10/a","curl","-s 192.168.1.10/a","'");
-		pb.inheritIO();
-		Process process=pb.start();
-		process.waitFor();
-		response.body(convertStreamToString(process.getInputStream()));
+//		ProcessBuilder pb = new ProcessBuilder("ssh", "glenn","<<","EOF","curl","-s 192.168.1.10/a\n",
+//				"curl","-s 192.168.1.10/a","EOF");
+//		pb.inheritIO();
+//		Process process=pb.start();
+//		process.waitFor();
+//		response.body(convertStreamToString(process.getInputStream()));
+		Process curl = Runtime.getRuntime().exec("ssh glenn \"curl -s 192.168.1.10/a && curl -s 192.168.1.10/b && curl -s 192.168.1.10/c\"");
+		byte[] buffer = new byte[1024];
+		curl.getInputStream().read(buffer);
+		String s = new String(buffer);
+		System.out.println(s);
+		response.body(s);
 		return response;
 	};
 	static String convertStreamToString(InputStream is) {

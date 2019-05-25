3cead536bef7dac2f596f7692e41eb81a7c1d148
jakob <jakob@JAKOB-HP>
2019-05-25 16:09:11 +0200

    Newcheck

--- src/main/java/gojb/jbhttp/API/GivareAPI.java
+++ src/main/java/gojb/jbhttp/API/GivareAPI.java
@@ -14,7 +14,7 @@ public class GivareAPI {
 		String[] splats=request2.splat();
 		String id=String.join("/",splats);
 		GivarObjekt objekt = givardata.get(id);
-		response2.body(objekt.data);
+		response2.body(objekt.readData());
 		return response2.body();
 	};
 	static Route write = (request2, response2) -> {
@@ -31,9 +31,17 @@ public class GivareAPI {
 	};
 }
 class GivarObjekt{
-	public String data;
-	public long tid = System.currentTimeMillis();
+	private String data;
+	private long tid = System.currentTimeMillis();
 	public GivarObjekt(String data) {
 		this.data=data;
+	}
+	public String readData() {
+		if (System.currentTimeMillis()<tid+1000*60*5) {//Newer than 5 minutes
+			return data;
+		}
+		else {
+			return "GAMMALT MÄTVÄRDE: "+data;
+		}
 	}
 }

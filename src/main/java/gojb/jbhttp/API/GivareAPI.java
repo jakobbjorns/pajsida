c31692a1ac9ace5b91a7304de44e791156be2334
jakob <jakob@JAKOB-HP>
2019-05-25 14:13:44 +0200

    Givarapi

---
+++ src/main/java/gojb/jbhttp/API/GivareAPI.java
@@ -0,0 +1,38 @@
+package gojb.jbhttp.API;
+
+import static spark.Spark.get;
+
+import java.util.HashMap;
+
+import spark.Route;
+import spark.RouteGroup;
+
+public class GivareAPI {
+	static HashMap<String, GivarObjekt> givardata = new HashMap<String, GivarObjekt>();
+
+	static Route read = (request2, response2) -> {
+		String[] splats=request2.splat();
+		String id=String.join("/",splats);
+		GivarObjekt objekt = givardata.get(id);
+		response2.body(objekt.data);
+		return response2.body();
+	};
+	static Route write = (request2, response2) -> {
+		String[] splats=request2.splat();
+		String id=String.join("/",splats);
+		String data=request2.body();
+		givardata.put(id,new GivarObjekt(data));
+		return request2;
+	};
+	public static RouteGroup givare = ()->{
+		get("/read/*", read);
+		get("/write/*",write);
+	};
+}
+class GivarObjekt{
+	public String data;
+	public long tid = System.currentTimeMillis();
+	public GivarObjekt(String data) {
+		this.data=data;
+	}
+}

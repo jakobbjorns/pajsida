9d5b750b3d62a08298b192069ae2703bac2b0de5
Jakob Björns <jakbjo@kth.se>
2019-11-16 21:28:16 +0100

    ute_fukt

--- public_html/index.js
+++ public_html/index.js
@@ -7,6 +7,7 @@ window.onload = function load() {
 	setdata("/F56/a","°C",0,"inne_temp")
 	setdata("/F56/b","%",0,"inne_fukt")
 	setdata("/F56/c","°C",1,"ute_temp")
+	setdata("/F56/c","°C",1,"ute_fukt")
 	setdata("/GU/givare/c","°C",1,"gu_ute_temp")
 	setdata("/GU/givare/d","%",0,"gu_ute_fukt")
 	setdata("/GU/givare/a","°C",1,"gu_inne_temp")
